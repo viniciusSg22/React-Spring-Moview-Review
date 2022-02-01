9997c588eedb4322db49777b36aae04ad18bc3fa
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-02-01 14:42:13 -0300

    Criando projeto

---
+++ server/src/test/java/com/vstech/moviesreview/MoviesReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vstech.moviesreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
