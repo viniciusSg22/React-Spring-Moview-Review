564f1e4f73dc3721f4c98b8e97aa6b79ccef2931
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-02-04 18:59:09 -0300

    Criando lógica com useParams para ver um único filme

--- client/src/pages/Review/review.tsx
+++ client/src/pages/Review/review.tsx
@@ -1,49 +1,11 @@
+import FormCard from "components/FormCard/formCard";
 import React from "react";
-import "pages/Review/review.css";
-import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Review = () => {
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
+  const params = useParams();
 
-  return (
-    <div className="form-container">
-      <img className="movie-card-image" src={movie.image} alt={movie.title} />
-      <div className="card-bottom-container">
-        <h3>{movie.title}</h3>
-        <form className="form">
-          <div className="form-group form-group">
-            <label htmlFor="email">Informe seu email</label>
-            <input type="email" className="form-control" id="email" />
-          </div>
-          <div className="form-group form-group">
-            <label htmlFor="score">Informe sua avaliação</label>
-            <select className="form-control" id="score">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
-          <div className="form-btn-container">
-            <button type="submit" className="btn btn-primary btn">
-              Salvar
-            </button>
-          </div>
-        </form>
-        <Link to="/">
-          <button className="btn btn-primary btn mt-3">Cancelar</button>
-        </Link>
-      </div>
-    </div>
-  );
+  return <FormCard movieId={`${params.movieId}`} />;
 };
 
 export default Review;
